2f30215ae3920ac204243fe3fba2a7511ac4a94b
divyanshujain11 <77712311+divyanshujain11@users.noreply.github.com>
2023-07-31 13:06:46 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -2,11 +2,11 @@
 import cv2
 import numpy
 cap=cv2.VideoCapture(0)
-status,photo=cap.read()
+status,image1=cap.read()
 cap.release()
-photo=image1[150:400,200:450]
-photo_resized = cv2.resize(photo, (200, 200))
-photo[0:200,0:200,:]=photo_resized
-cv2.imshow("merge_photo",photo)
+photo1=image1[150:400,200:450]
+photo1_resized = cv2.resize(photo1, (200, 200))
+image1[0:200,0:200,:]=photo1_resized
+cv2.imshow("merge_photo",image1)
 cv2.waitKey(5000)
 cv2.destroyAllWindows()
