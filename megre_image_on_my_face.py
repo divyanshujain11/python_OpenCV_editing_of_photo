b2344c55dbfc5b23be19c2819e54f0f4569493c8
divyanshujain11 <77712311+divyanshujain11@users.noreply.github.com>
2023-07-31 13:07:06 +0530

    Update megre_image_on_my_face.py

--- megre_image_on_my_face.py
+++ megre_image_on_my_face.py
@@ -1,15 +1,13 @@
-#black glass image
-import cv2
-import numpy
+#black glass image 
 cap=cv2.VideoCapture(0)
-status,photo=cap.read()
+status,photo2=cap.read()
 cap.release()
-image2=photo
-blaCK_GLASS=cv2.imread("black_sunglass.jpg")
-photo=blaCK_GLASS[200:450,800:1450,]
+image2=photo2
+blCK_GLS=cv2.imread("black_sunglass.jpg")
+photo2=blCK_GLS[200:450,800:1450,]
 # print(numpydata.shape)
-photo_resized = cv2.resize(photo, (200, 50))
-photo[200:250,320:520,:]=photo_resized
-cv2.imshow("merge_photo_sunglass",photo)
+photo2_resized = cv2.resize(photo2, (200, 50))
+image2[200:250,320:520,:]=photo2_resized
+cv2.imshow("merge_photo_sunglass",image2)
 cv2.waitKey()
 cv2.destroyAllWindows()
